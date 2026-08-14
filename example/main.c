c37bc895a4f6983454de83e336848bae74fd3a89
Rakesh Kumar R <65564407+rakeshkumarr4@users.noreply.github.com>
2026-08-14 15:31:23 +0530

    Example implementation of I2C initialization and LCD display
    Initialize I2C for LCD display and print 'Hello World'.

---
+++ example/main.c
@@ -0,0 +1,57 @@
+#include "stm32f4xx.h"
+#include "i2c.h"
+#include "lcd.h"
+
+void I2C_Init(void)
+{
+    /* Enable I2C1 and GPIOB clocks */
+    RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
+
+    /* PB8 = SCL, PB9 = SDA */
+    GPIOB->MODER &= ~(3U << (8 * 2));
+    GPIOB->MODER |= (2U << (8 * 2));
+    GPIOB->OTYPER |= (1U << 8);
+    GPIOB->PUPDR &= ~(3U << (8 * 2));
+    GPIOB->AFR[1] &= ~(0xF << ((8 - 8) * 4));
+    GPIOB->AFR[1] |= (4U << ((8 - 8) * 4));
+
+    GPIOB->MODER &= ~(3U << (9 * 2));
+    GPIOB->MODER |= (2U << (9 * 2));
+    GPIOB->OTYPER |= (1U << 9);
+    GPIOB->PUPDR &= ~(3U << (9 * 2));
+    GPIOB->AFR[1] &= ~(0xF << ((9 - 8) * 4));
+    GPIOB->AFR[1] |= (4U << ((9 - 8) * 4));
+
+    /* Disable I2C before configuring */
+    I2C1->CR1 &= ~I2C_CR1_PE;
+
+    /* Configure I2C clock: 16 MHz / 80 = 200kHz approx */
+    I2C1->CR2 = 16;
+    I2C1->CCR = 80;
+    I2C1->TRISE = 17;
+
+    /* Enable I2C */
+    I2C1->CR1 |= I2C_CR1_PE;
+}
+
+int main(void)
+{
+    I2C_Init();
+
+    /* Address, rows, columns */
+    I2C_LCD_Init(0x23, 2, 16);
+
+    /* Turn cursor on */
+    show_cursor();
+
+    /* Print text */
+    LCD_putstr("Hello");
+    move_to(0, 1);
+    LCD_putstr("World");
+
+    while (1)
+    {
+        /* Application loop */
+    }
+}
